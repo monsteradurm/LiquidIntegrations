0f703af5ab6d95dcd57e72483a45fa25928b7d5e
monsteradurm <acranch@liquidanimation.com>
2022-10-07 14:30:19 +1000

    feedback department

--- src/controllers/monday-controller.js
+++ src/controllers/monday-controller.js
@@ -78,10 +78,11 @@ async function StoreBoardItemStatus(req, res) {
 
     try{
       const mondayItem = await mondayService.getItemInfo(itemId);
-      let subitem_index = mondayHelper.ParseMaxSubitemValue(mondayItem, 'Index', 'text') + 1;
+      let subitem_index = mondayHelper.ParseMaxSubitemValue(mondayItem, 'Index', 'text');
       let review = _.find(mondayItem.subitems, s => mondayHelper.ParseColumnValue(s, 'Index', 'text') === subitem_index);
       const department = mondayHelper.ParseColumnValue(review, 'Feedback Department', 'text')
 
+      console.log("\n\nFound Feedback Department: " + department);
       if (department)
         data['department'] = department;
     } catch (err) {
