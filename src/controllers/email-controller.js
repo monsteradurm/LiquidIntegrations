d26e0295ecab5e7149132e9147a70ab6058f1c00
monsteradurm <acranch@liquidanimation.com>
2022-11-03 08:19:17 +1000

    Email Attachments

--- src/controllers/email-controller.js
+++ src/controllers/email-controller.js
@@ -6,8 +6,8 @@ const { TRANSFORMATION_TYPES } = require('../constants/transformation');
 
 async function Email(req, res) {
     try {
-        const { toAddress, subject, text, html } = req.body;
-        const result = await emailService.sendEmail( toAddress, subject, text, html );
+        const { toAddress, subject, text, html, attachments } = req.body;
+        const result = await emailService.sendEmail( toAddress, subject, text, html, attachments );
         return res.status(200).send({})
     }
     catch (err) {
