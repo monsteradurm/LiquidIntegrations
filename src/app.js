663846065582cacca5c6142725ebd01a4dabd98e
monsteradurm <acranch@liquidanimation.com>
2022-11-18 11:47:45 +1000

    bodyparser limit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const routes = require('./routes');
 const { createTunnel } = require('./helpers/tunnel');
 
 const { PORT: port } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(routes);
 app.listen(port, () => {
   createTunnel(port);
